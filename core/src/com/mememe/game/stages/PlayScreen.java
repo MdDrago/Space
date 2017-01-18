fe42efb8e0e25a4703c399c9e027403a86154536
MdDrago <westheimer@ukr.net>
2017-01-18 16:11:53 +0200

    Rebild

--- core/src/com/mememe/game/stages/PlayScreen.java
+++ core/src/com/mememe/game/stages/PlayScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.mememe.game.Game;
 import com.mememe.game.gameplay.Ship;
 
@@ -16,58 +17,20 @@ import com.mememe.game.gameplay.Ship;
 
 public class PlayScreen extends DefaultScreen{
     private Image bg;
-    private Stage stage;
+    private Ship ship;
 
-    public PlayScreen(Game game) {
-        super(game);
-        stage = new Stage();
+    public PlayScreen(Game game, Viewport viewport) {
+        super(game, viewport);
         bg = new Image(new Texture("backGround.jpg"));
-        Gdx.input.setInputProcessor(stage);
-
-        stage.addActor(bg);
-        stage.addActor(new Ship());
+        Gdx.input.setInputProcessor(this);
+        ship = new Ship();
+        setActors();
     }
 
-    @Override
-    public void show() {
 
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
-
-       /* game.batch.begin();
-        game.batch.draw(bg,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-        game.batch.end();*/
-
-        stage.act(delta);
-        stage.draw();
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
+    private void setActors(){
+        this.addActor(bg);
+        this.addActor(ship);
     }
 
     @Override
